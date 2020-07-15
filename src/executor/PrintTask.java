c240227d81799121939a3559e87273016840da09
Jitendra Kumar Yadav <jiteyad@gmail.com>
2020-07-15 02:26:09 +0200

    update print task

--- src/executor/PrintTask.java
+++ src/executor/PrintTask.java
@@ -2,21 +2,21 @@ package executor;
 
 import java.util.Random;
 
-public class PrintTask implements Runnable{
-    private static Random generator = new Random();
+public class PrintTask implements Runnable {
+    private static final Random generator = new Random();
     private static int instanceNr = 1;
-    private int sleepTime;
-    private String threadName;
+    private final int sleepTime;
+    private final String threadName;
 
     public PrintTask() {
-        threadName = "PrintTask "+ instanceNr++ ;
+        threadName = "PrintTask " + instanceNr++;
         sleepTime = generator.nextInt(5000);
     }
 
     @Override
     public void run() {
         try {
-            System.out.printf("%s sleeps for %d millisec \n",threadName,sleepTime);
+            System.out.printf("%s sleeps for %d millisec \n", threadName, sleepTime);
             Thread.sleep(sleepTime);
         } catch (InterruptedException e) {
             e.printStackTrace();
